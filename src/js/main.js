620101d8795c3e0359338ab107d172a627696a7c
eslamabdo164 <eslam.abdoweb@hotmail.com>
2019-05-21 20:16:12 +0200

    fixing mobile view

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// change Navbar when scroll
+
+$(window).scroll(function(){
+    var scrolTop = $(window).scrollTop();
+    // TODO : GET the ScrolTop value and change Style Based On It
+
+    console.log(scrolTop)
+})
